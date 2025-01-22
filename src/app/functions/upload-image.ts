f44d2f60490a17d7c0be3024463e26208c2cc222
Ronald Camargo <ronaldcamargodev@gmail.com>
2025-01-21 22:50:35 -0400

    metodo error handling

--- src/app/functions/upload-image.ts
+++ src/app/functions/upload-image.ts
@@ -1,7 +1,10 @@
 import { Readable } from 'node:stream'
 import { db } from '@/infra/db'
 import { schema } from '@/infra/db/schemas'
+import type { Either } from '@/shared/either'
+import { makeLeft, makeRight } from '@/shared/either'
 import { z } from 'zod'
+import { InvalidFileFormat } from './errors/invalid-file-format'
 
 const uploadImageInput = z.object({
   fileName: z.string(),
@@ -13,11 +16,13 @@ type UploadImageInput = z.input<typeof uploadImageInput>
 
 const allowedMimeTypes = ['image/jpg', 'image/jpeg', 'image/png', 'image/webp']
 
-export async function uploadImage(input: UploadImageInput) {
+export async function uploadImage(
+  input: UploadImageInput
+): Promise<Either<InvalidFileFormat, { url: string }>> {
   const { contentStream, contentType, fileName } = uploadImageInput.parse(input)
 
   if (!allowedMimeTypes.includes(contentType)) {
-    throw new Error('Invalid file input')
+    return makeLeft(new InvalidFileFormat())
   }
 
   // TODO: carregar a imagem p/ o Cloudflare R2
@@ -27,4 +32,6 @@ export async function uploadImage(input: UploadImageInput) {
     remoteKey: fileName,
     remoteUrl: fileName,
   })
+
+  return makeRight({ url: '' })
 }
